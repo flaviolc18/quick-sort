cc730d47c724f906d48030912e0bcd54092f7d15
= <flaviolc18@gmail.com>
2019-10-15 15:35:21 -0300

    dynamic programming algorithm implemented

---
+++ paradigms/doc/DOC.md
@@ -0,0 +1,22 @@
+### <center> Trabalho Estrutura de Dados</center>
+
+#### <center>Flávio Lúcio Corrêa Júnior </center>
+
+**<center>Universidade Federal de Minas Gerais (UFMG) </center>**
+**<center>Belo Horizonte - MG - Brasil</center>**
+
+---
+
+- ### **Sumário**:
+
+  **1. Introdução**
+
+  **2. Implementação**
+
+  **3. Análise de complexidade**
+
+  **4. Conclusão**
+
+  **5. Bibliografia**
+
+---
---
+++ paradigms/src/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+std::pair<int, int> solve_dp(int n, int W, int Vn[], int Wn[])
+{
+  // OBS: as posições 0 das duas dimensões são usadas para os casos base
+  // ou seja, os arranjos de soluções de fato começam nas posições 1
+  // para compensar isso, ao acessar os arranjos Vn e Wn (os quais comçam em 0) usamos i - 1
+  int opt[n + 1][W + 1];
+
+  for (int i = 0; i <= n; i++)
+  {
+    for (int w = 0; w <= W; w++)
+    {
+      // casos base
+      if (i == 0 || w == 0)
+        opt[i][w] = 0;
+      // se o peso for maior que o total permitido não o incluimos
+      else if (Wn[i - 1] > w)
+        opt[i][w] = opt[i - 1][w];
+      else
+        // caso contrário pegamos o máximo entre a solução q inclui e a q não inclui Vn[i]
+        // discontando Wn[i] do máximo permitido
+        opt[i][w] = std::max(Vn[i - 1] + opt[i - 1][w - Wn[i - 1]], opt[i - 1][w]);
+    }
+  }
+
+  int res = opt[n][W];
+  int items = 0;
+  int w = W;
+  for (int i = n; i > 0 && res > 0; i--)
+  {
+    if (res == opt[i - 1][w])
+      continue;
+    else
+    {
+      items++;
+      res = res - Vn[i - 1];
+      w = w - Wn[i - 1];
+    }
+  }
+
+  return std::make_pair(opt[n][W], items);
+}
+
+int main(int argc, char **argv)
+{
+  if (argc < 2)
+    return 0;
+
+  int available_cash;
+  int qtd_islands;
+
+  std::ifstream file;
+  file.open(argv[1]);
+
+  file >> available_cash >> qtd_islands;
+
+  int weights[qtd_islands];
+  int points[qtd_islands];
+  for (int i = 0; i < qtd_islands; i++)
+    file >> weights[i] >> points[i];
+
+  file.close();
+
+  std::pair<int, int> res_dp = solve_dp(qtd_islands, available_cash, points, weights);
+  std::cout << res_dp.first << " " << res_dp.second << std::endl;
+
+  return 0;
+}
