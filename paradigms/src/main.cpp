4966ab0e1fe5ec6b0114aa228b0ecacfb29a5611
= <flaviolc18@gmail.com>
2019-10-21 19:18:26 -0300

    script de testes

--- paradigms/src/main.cpp
+++ paradigms/src/main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <vector>
 #include <algorithm>
 
 std::pair<int, int> solve_dp(int n, int W, int Vn[], int Wn[])
@@ -45,6 +43,35 @@ std::pair<int, int> solve_dp(int n, int W, int Vn[], int Wn[])
   return std::make_pair(opt[n][W], items);
 }
 
+bool compare(std::pair<int, int> const &i1, std::pair<int, int> const &i2)
+{
+  return ((double)i1.first / i1.second > (double)i2.first / i2.second);
+}
+
+std::pair<int, int> solve_greedy(int n, int W, int Vn[], int Wn[])
+{
+  std::pair<int, int> sorted[n];
+  for (int i = 0; i < n; i++)
+    sorted[i] = std::make_pair(Vn[i], Wn[i]);
+
+  std::sort(sorted, sorted + n, compare);
+
+  int days = 0, pts = 0, cash = W;
+  for (int i = 0; i < n && cash > 0; i++)
+  {
+    int val = sorted[i].first, cost = sorted[i].second;
+
+    if (cost <= cash)
+    {
+      days += cash / cost;
+      pts += val * (cash / cost);
+      cash %= cost;
+    }
+  }
+
+  return std::make_pair(pts, days);
+}
+
 int main(int argc, char **argv)
 {
   if (argc < 2)
@@ -58,14 +85,17 @@ int main(int argc, char **argv)
 
   file >> available_cash >> qtd_islands;
 
-  int weights[qtd_islands];
+  int costs[qtd_islands];
   int points[qtd_islands];
   for (int i = 0; i < qtd_islands; i++)
-    file >> weights[i] >> points[i];
+    file >> costs[i] >> points[i];
 
   file.close();
 
-  std::pair<int, int> res_dp = solve_dp(qtd_islands, available_cash, points, weights);
+  std::pair<int, int> res_greedy = solve_greedy(qtd_islands, available_cash, points, costs);
+  std::cout << res_greedy.first << " " << res_greedy.second << std::endl;
+
+  std::pair<int, int> res_dp = solve_dp(qtd_islands, available_cash, points, costs);
   std::cout << res_dp.first << " " << res_dp.second << std::endl;
 
   return 0;
